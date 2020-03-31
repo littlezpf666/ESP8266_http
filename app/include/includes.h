a68f8e880863cadd6502ca8049e098cf26d8ce61
littlezpf666 <1613034482@qq.com>
2020-03-31 20:26:00 +0800

    纠错

---
+++ app/include/includes.h
@@ -0,0 +1,20 @@
+/*
+ * includes.c
+ *
+ *  Created on: 2019Äê12ÔÂ12ÈÕ
+ *      Author: 16130
+ */
+#ifndef APP_USER_INCLUDES_H_
+#define APP_USER_INCLUDES_H_
+#include "ets_sys.h"
+#include "osapi.h"
+
+#include "c_types.h"
+#include "mem.h"
+#include "string.h"
+//#include "espconn.h"
+
+#include "user_interface.h"
+
+
+#endif
